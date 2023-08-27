2e7370cef8ce48156e16f6aef67a17c397f3a2dd
AviouslyK <avi.fs.kahn@gmail.com>
2023-08-27 14:42:16 -0400

    why can't git load the js file
     let's try putting it in the same dir

---
+++ docs/work_samples/semantle.js
@@ -0,0 +1,34 @@
+const guessInput = document.getElementById("guessInput");
+const guessButton = document.getElementById("guessButton");
+const outputArea = document.getElementById("outputArea");
+
+guessButton.addEventListener("click", () => {
+    const guess = guessInput.value;
+    
+    // Send the guess to the server using fetch
+    const SERVER_URL = 'http://13.58.95.29:5000';  // AWS EC2 instance I've set up
+
+guessButton.addEventListener("click", () => {
+    const guess = guessInput.value;
+
+    fetch(`${SERVER_URL}/process_guess`, {
+        method: 'POST',
+        body: JSON.stringify({ guess }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        outputArea.textContent = `Similarity Score: ${data.score}`;
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });    
+});
+});
